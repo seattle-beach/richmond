6396ce4c1e50c555098e89a4b02513a95f712eb3
Augustus Lidaka, Beatrice Speck and Ross Greenwood <pair+alidaka+bspeck+rgreenwood@pivotal.io>
2017-01-10 10:54:28 -0800

    WIP: adding tests

---
+++ test/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void ReturnsResponse() 
+        {
+        	var richmond = new Richmond();
+        	var response = richmond.HelloWorld();
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            // body is like JSON: {"response":"hello, world!"}
+        }
+    }
+}
