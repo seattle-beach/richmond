1c68cc9bfc9ac919a465313adaf5193056f1690a
Beatrice Speck <bspeck@pivotal.io>
2017-01-12 15:43:59 -0800

    Add Food truck info endpoint: * Scrape food truck site. * Add development debug configuration.
    [#137372539]
    
    Signed-off-by: Ross Greenwood <rgreenwood@pivotal.io>

---
+++ backend/test/FoodTruckControllerTests.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using System.Collections.Generic;
+using LightMock; 
+
+namespace Richmond.Tests
+{
+    public class FoodTruckControllerTests
+    {
+        [Fact]
+        public void ReturnsNonemptyHttpResponse() 
+        {
+            var mockContext = new MockContext<IFoodTruckRepository>();
+            var foodTruckRepositoryMock = new FoodTruckRepositoryMock(mockContext);
+            mockContext.Arrange(f => f.RequestFoodTruckWebsite()).Returns("");
+            mockContext.Arrange(f => f.ParseFoodTruckSite("")).Returns(new FoodTruckResponse.FoodTruck[4]);
+
+            // inject a thing which gets the web site data for us (here, mock the return value)
+            var subject = new FoodTruckController(foodTruckRepositoryMock);
+
+            var result = subject.FoodTrucks();
+
+            var response = result as OkObjectResult;
+            Assert.NotNull(response);
+
+            var payload = response.Value as FoodTruckResponse;
+            Assert.NotNull(payload);
+
+            Assert.Equal(4, payload.FoodTrucks.Count);
+        }
+
+    }
+
+    public class FoodTruckRepositoryMock : IFoodTruckRepository
+    {
+        private readonly IInvocationContext<IFoodTruckRepository> context;
+        public FoodTruckRepositoryMock(IInvocationContext<IFoodTruckRepository> context)
+        {
+            this.context = context;
+        }
+
+        public IList<FoodTruckResponse.FoodTruck> ParseFoodTruckSite(string html)
+        {
+            return context.Invoke(f => f.ParseFoodTruckSite(html));
+        }  
+        public string RequestFoodTruckWebsite()
+        {
+            return context.Invoke(f => f.RequestFoodTruckWebsite());
+        }    
+    }
+}
