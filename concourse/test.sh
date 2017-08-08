7755829563022526b1216bda69432d1bc8387107
Gregg Van Hove <gvanhove@pivotal.io>
2017-08-08 12:25:43 -0700

    Fix folders and log exactly what we need

--- concourse/test.sh
+++ concourse/test.sh
@@ -1,10 +1,13 @@
-#!/bin/bash -ev
+#!/bin/bash -e
 
 project_root=$1
 
 cd $project_root
 cd src
+echo "Restoring src..."
 dotnet restore
-cd test-backend
+cd ../test-backend
+echo "Restoring tests..."
 dotnet restore
+echo "Running tests..."
 dotnet test
